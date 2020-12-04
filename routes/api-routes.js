8a126b1c708b44bf3b640e715ca222977426e68d
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-12-03 23:02:54 -0500

    added api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,63 @@
 const router = require("express").Router();
+const Workout = require("../models/workout");
 
-//get api/workout
+//post request
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
-//
+// put request
+router.put("/api/workout/:id", ({ body, params } , res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+//get request
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+// get request (7 day range)
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find().limit(7)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
 
 module.exports = router;
